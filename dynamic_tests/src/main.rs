c81a064cc7a2333363da43334613c09688ca4105
David Young <david.young@nccgroup.com>
2021-01-29 11:57:48 +0000

    Add additional test cases

--- dynamic_tests/src/main.rs
+++ dynamic_tests/src/main.rs
@@ -10,7 +10,10 @@ use web_archive::parsing::Resource;
 mod pages;
 
 fn server() -> rocket::Rocket {
-    rocket::ignite().mount("/", rocket::routes![index, style])
+    rocket::ignite().mount(
+        "/",
+        rocket::routes![index, style, rust_logo, js, blog, ferris],
+    )
 }
 
 fn main() {
@@ -22,6 +25,9 @@ fn main() {
 
     // Start running the tests
     test_index();
+    test_blog();
+
+    println!("Success! All dynamic tests have passed");
 }
 
 fn test_index() {
@@ -38,4 +44,21 @@ fn test_index() {
         &Resource::Css(style().to_string())
     );
     println!("[PASS] Index page with CSS");
+}
+
+fn test_blog() {
+    let u = "http://localhost:8000/pages/blog.html";
+
+    let a = blocking::archive(u).unwrap();
+
+    assert_eq!(a.content, blog());
+    assert_eq!(a.resource_map.len(), 4);
+
+    assert_eq!(
+        a.resource_map
+            .get(&Url::parse("http://localhost:8000/style.css").unwrap())
+            .unwrap(),
+        &Resource::Css(style().to_string())
+    );
+    println!("[PASS] Blog page with multiple resources");
 }
--- dynamic_tests/src/pages.rs
+++ dynamic_tests/src/pages.rs
@@ -11,9 +11,55 @@ pub fn index() -> &'static str {
 	</html>"#
 }
 
+#[get("/pages/blog.html")]
+pub fn blog() -> &'static str {
+    r#"
+	<html>
+		<head>
+			<title>Blog!!</title>
+			<link rel="stylesheet" href="/style.css" />
+			<script language="javascript" src="../scripts/1.js"></script>
+		</head>
+		<body>
+			<div>
+				<p>
+					<img src="/images/rust-logo-blk.svg" />
+				</p>
+				<div>
+					<div>
+						<img src="../images/rustacean-flat-happy.png" />
+						<p>
+							<img src="notfound.jpg" />
+						</p>
+					</div>
+				</div>
+			</div>
+		</body>
+	</html>
+	"#
+}
+
 #[get("/style.css")]
 pub fn style() -> &'static str {
     r#"body {
 		background-color: blue;
 	}"#
+}
+
+#[get("/images/rust-logo-blk.svg")]
+pub fn rust_logo() -> &'static str {
+    include_str!("../resources/rust-logo-blk.svg")
+}
+
+#[get("/images/rustacean-flat-happy.png")]
+pub fn ferris() -> &'static [u8] {
+    include_bytes!("../resources/rustacean-flat-happy.png")
+}
+
+#[get("/scripts/1.js")]
+pub fn js() -> &'static str {
+    r#"function js_function() {
+		console.log("Here is some javascript!");
+	}
+	"#
 }
